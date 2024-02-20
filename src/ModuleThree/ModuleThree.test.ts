08457dc73ebc4d59ac6ccf72b5ac085d287c003b
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-02-19 22:41:05 -0300

    test: ensure that an valid transitions parameter is provided to ModuleThree

--- src/ModuleThree/ModuleThree.test.ts
+++ src/ModuleThree/ModuleThree.test.ts
@@ -1,7 +1,16 @@
 import { ModuleThree } from ".";
+import { STATES, TRANSITIONS } from "../utils/constants";
 
 describe("ModuleThree", () => {
   test("should throw an error if no valid states is provided", () => {
-    expect(() => new ModuleThree([])).toThrow("Invalid states parameter");
+    expect(() => new ModuleThree([], TRANSITIONS)).toThrow(
+      "Invalid states parameter"
+    );
+  });
+
+  test("should throw an error if no transitions states is provided", () => {
+    expect(() => new ModuleThree(STATES, {})).toThrow(
+      "Invalid transitions parameter"
+    );
   });
 });
