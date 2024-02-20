f5273e85d1b4fb59317ac42bef23ae03c83ff8ae
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-02-19 21:49:13 -0300

    test: ensure that an valid transitions parameter is provided

--- src/MachineSteps/MachineSteps.test.ts
+++ src/MachineSteps/MachineSteps.test.ts
@@ -1,7 +1,21 @@
 import { MachineSteps } from ".";
 
+const TRANSITIONS = {
+  S0: { "0": "S0", "1": "S1" },
+  S1: { "0": "S2", "1": "S0" },
+  S2: { "0": "S1", "1": "S2" },
+};
+
 describe("MachineSteps", () => {
   test("should throw an error if no valid states is provided", () => {
-    expect(() => new MachineSteps([])).toThrow("Invalid states parameter");
+    expect(() => new MachineSteps([], TRANSITIONS)).toThrow(
+      "Invalid states parameter"
+    );
+  });
+
+  test("should throw an error if no valid transitions is provided", () => {
+    expect(() => new MachineSteps(["A", "B"], {})).toThrow(
+      "Invalid transitions parameter"
+    );
   });
 });
