28d58fd5ae3a7914e53997c81d1824073b12cc03
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-02-19 23:03:51 -0300

    refactor: add message when no provided a parameter to InvalidParameterError

--- src/erros/InvalidParameterError.ts
+++ src/erros/InvalidParameterError.ts
@@ -1,6 +1,6 @@
 export class InvalidParameterError extends Error {
-  constructor(paramName: string) {
-    super(`Invalid ${paramName} parameter`);
+  constructor(paramName?: string) {
+    super(paramName ? `Invalid ${paramName} parameter` : "Invalid parameter");
     this.name = "InvalidParameterError";
   }
 }
