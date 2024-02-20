cf589274c814459aa72a26fa857383ab7d3b553e
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-02-19 22:41:43 -0300

    feat: create a validation to check if transitions parameter is valid

--- src/ModuleThree/index.ts
+++ src/ModuleThree/index.ts
@@ -2,11 +2,18 @@ import { InvalidParameterError } from "../erros/InvalidParameterError";
 
 export class ModuleThree {
   states: IStates;
+  transitions: ITransitions;
 
-  constructor(states: IStates) {
+  constructor(states: IStates, transitions: ITransitions) {
     if (!states || !states.length) {
       throw new InvalidParameterError("states");
     }
+
+    if (!transitions || !Object.values(transitions).length) {
+      throw new InvalidParameterError("transitions");
+    }
+
     this.states = states;
+    this.transitions = transitions;
   }
 }
