c48c31c120f596226e107e6d886c63bddb7290ff
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-02-19 22:45:49 -0300

    feat: create a validation to check if alphabet parameter is valid

--- src/ModuleThree/index.ts
+++ src/ModuleThree/index.ts
@@ -3,10 +3,15 @@ import { InvalidParameterError } from "../erros/InvalidParameterError";
 export class ModuleThree {
   states: IStates;
   transitions: ITransitions;
+  alphabet: string[];
 
-  constructor(states: IStates, transitions: ITransitions) {
+  constructor(states: IStates, transitions: ITransitions, alphabet: string[]) {
     if (!states || !states.length) {
       throw new InvalidParameterError("states");
+    }
+
+    if (!alphabet || !alphabet.length) {
+      throw new InvalidParameterError("alphabet");
     }
 
     if (!transitions || !Object.values(transitions).length) {
@@ -15,5 +20,6 @@ export class ModuleThree {
 
     this.states = states;
     this.transitions = transitions;
+    this.alphabet = alphabet;
   }
 }
