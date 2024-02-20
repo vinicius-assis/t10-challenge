9e50771bc11c414168cb536f784e082222222eef
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-02-19 21:49:37 -0300

    feat: create a validation to check if transitions is a valid parameter

--- src/MachineSteps/index.ts
+++ src/MachineSteps/index.ts
@@ -1,10 +1,18 @@
+type ITransitions = { [key: string]: { [key: string]: string } };
+
 export class MachineSteps {
   states: string[];
-  constructor(states: string[]) {
+  transitions: ITransitions;
+  constructor(states: string[], transitions: ITransitions) {
     if (!states || !states.length) {
       throw new Error("Invalid states parameter");
     }
 
+    if (!transitions || !Object.values(transitions).length) {
+      throw new Error("Invalid transitions parameter");
+    }
+
     this.states = states;
+    this.transitions = transitions;
   }
 }
